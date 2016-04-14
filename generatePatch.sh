13c74c8b21abd93f326bfdcf4b59f0bec9ff62a6
Jake <jake@unimac>
2016-04-14 12:29:43 +0100

    include wolfssl patche and mod gen script

--- generatePatch.sh
+++ generatePatch.sh
@@ -2,6 +2,11 @@
 # the patch file
 
 : > openssl.patch
-for dfile in $(find . -iname '*_diff'); do
+for dfile in $(find ./openssl -iname '*_diff'); do
   diff -u "${dfile%_diff}" "$dfile" >> openssl.patch
+done
+
+: > wolfssl.patch
+for dfile in $(find ./wolfssl -iname '*_diff'); do
+  diff -u "${dfile%_diff}" "$dfile" >> wolfssl.patch
 done
