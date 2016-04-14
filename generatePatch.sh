20352c9613753bc78fc7ebfa36bb4343cda46047
Jake <jake@unimac>
2016-04-14 16:04:07 +0100

    add more to patches

--- generatePatch.sh
+++ generatePatch.sh
@@ -1,12 +1,14 @@
 # find all the files with suffix '_diff' and use the diff command to generate
 # the patch file
 
+git submodule foreach git reset --hard
+
 : > openssl.patch
 for dfile in $(find ./openssl -iname '*_diff'); do
-  diff -u "${dfile%_diff}" "$dfile" >> openssl.patch
+  diff -Nu "${dfile%_diff}" "$dfile" >> openssl.patch
 done
 
 : > wolfssl.patch
 for dfile in $(find ./wolfssl -iname '*_diff'); do
-  diff -u "${dfile%_diff}" "$dfile" >> wolfssl.patch
+  diff -Nu "${dfile%_diff}" "$dfile" >> wolfssl.patch
 done
