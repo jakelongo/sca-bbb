8a73abaf095c7b51c78e401c428edbb782312189
Jake <jake@unimac>
2016-05-09 16:54:01 +0100

    add guards to hearder

--- neon/fvr-data/trigger.h
+++ neon/fvr-data/trigger.h
@@ -1,7 +1,10 @@
-// trigger header file
+#ifndef __TRIGGER_H__
+#define __TRIGGER_H__
 
 int trigger_init(void);
 int trigger_setup(void);
 int trigger_close(void);
 int trigger_set(void);
 int trigger_clr(void);
+
+#endif /* __TRIGGER_H__ */
