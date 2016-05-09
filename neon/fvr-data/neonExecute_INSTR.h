c37bc39a436942f1eb02a3f0d0b08bcb80c19439
Jake <jake@unimac>
2016-05-09 13:11:10 +0100

    fix arg type

--- neon/fvr-data/neonExecute_INSTR.h
+++ neon/fvr-data/neonExecute_INSTR.h
@@ -3,10 +3,10 @@
 
 // All dynamically generated functions
 
-void neonExecute_vmulu32(uint32_t** memBank);
-void neonExecute_vsubu32(uint32_t** memBank);
-void neonExecute_vaddu32(uint32_t** memBank);
-void neonExecute_veoru32(uint32_t** memBank);
-void neonExecute_vcequ32(uint32_t** memBank);
+void neonExecute_vmulu32(uint8_t** memBank);
+void neonExecute_vsubu32(uint8_t** memBank);
+void neonExecute_vaddu32(uint8_t** memBank);
+void neonExecute_veoru32(uint8_t** memBank);
+void neonExecute_vcequ32(uint8_t** memBank);
 
 #endif /* __neonExecute_INSTR_H__ */
