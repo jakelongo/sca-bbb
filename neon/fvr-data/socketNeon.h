025ec9444129970803731712e943e7056d222a67
Jake <jake@unimac>
2016-05-09 16:17:04 +0100

    remove old decl from header

--- neon/fvr-data/socketNeon.h
+++ neon/fvr-data/socketNeon.h
@@ -1,5 +1,5 @@
 #ifndef __SOCKETAES_H__
-# define __SOCKETAES_H__ 
+# define __SOCKETAES_H__
 
 #include <stdint.h>
 
@@ -11,11 +11,6 @@ typedef union bitpacketUnion {
 
 uint32_t getData(int sessionfd, char* dest, uint32_t maxBuffer);
 uint32_t putData(int sessionfd, char* src, uint32_t buffLen);
-
-extern execNeon(uint8_t** memBank, uint32_t opIndex);
-extern execNeon16(uint8_t** memBank, uint32_t opIndex);
-extern execNeon32(uint8_t** memBank, uint32_t opIndex);
-
 
 
 #endif /* __SOCKETAES_H__ */
